e94d82eb96dafa0d06d093f160ceae4ab57bbaa9
mys <mys@MYS>
2020-06-23 13:48:55 +0900

    최초등록

---
+++ hello-word/src/test/java/com/reonsoft/helloword/HelloWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reonsoft.helloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
